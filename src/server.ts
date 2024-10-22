b0fec797637c149cfd208e52e398362dcce7e492
Maruf Ahmed <marufdev09@gmail.com>
2024-10-22 10:22:50 +0600

    3rd-commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 import config from "./config";
-const port = 3000
 
 
 async function main() {
@@ -9,7 +8,7 @@ async function main() {
         await mongoose.connect(config.db_url as string);
   
 
-        app.listen(port, () => {
+        app.listen(config.port, () => {
             console.log(`Example app listening on port ${config.port}`)
           })
           

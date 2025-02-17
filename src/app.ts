283b36fb29c7bb099157c6bf01d80f8eff93b342
Maruf Ahmed <109242141+maufdev09@users.noreply.github.com>
2025-02-17 15:49:24 +0000

    v1

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import globalErrorHandler from './middleware/globalErrorHandler'
 import notFound from './middleware/notFound'
+import router from './routes'
 export const app = express()
 
 app.use(express.json())
+
+
+app.use('/api/v1', router );
 
 
 app.get('/', (req, res) => {
